d8bb927019433f6f773874ac0107df3342264ee9
Jose Aviles <na@na.na>
2020-12-27 13:16:44 -0400

    Fix arch-chroot commands

--- test/arch_test_install.sh
+++ test/arch_test_install.sh
@@ -10,7 +10,7 @@ mkfs.ext4 /dev/sda2
 
 partition
 mount /dev/sda2 /mnt
-pacstrap /mnt base linux linux-firmware grub
+pacstrap /mnt base linux linux-firmware grub vim man-db
 genfstab -L /mnt >> /mnt/etc/fstab
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/EST /etc/localtime
 arch-chroot /mnt hwclock --systohc
@@ -21,10 +21,16 @@ cat > /mnt/etc/hosts <<- EOM
 ::1		localhost
 127.0.1.1	runner.localdomain	runner
 EOM
-arch-chroot /mnt passwd
 cat > /mnt/root/script.sh <<- EOM
 grub-install --target=i386-pc --recheck /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
+systemctl enable systemd-networkd.service
+systemctl enable systemd-resolved.service
+echo "[Match]" >> /etc/systemd/network/20-wired.network
+echo "Name=enp0s3" >> /etc/systemd/network/20-wired.network
+echo "[Network]" >> /etc/systemd/network/20-wired.network
+echo "DHCP=yes" >> /etc/systemd/network/20-wired.network
 EOM
 chmod +x /mnt/root/script.sh
 arch-chroot /mnt /root/script.sh
+arch-chroot /mnt passwd
